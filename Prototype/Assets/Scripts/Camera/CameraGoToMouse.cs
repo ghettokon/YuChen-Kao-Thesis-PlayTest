163c8b9c76df529c9dc8674f3fab7742e90ca051
lindayckao <lindayckao@gmail.com>
2014-11-03 23:18:40 -0500

    added Mathf.Clamp to CameraGoToMouse

--- Prototype/Assets/Scripts/Camera/CameraGoToMouse.cs
+++ Prototype/Assets/Scripts/Camera/CameraGoToMouse.cs
@@ -18,5 +18,10 @@ public class CameraGoToMouse : MonoBehaviour {
 			target.z = transform.position.z;
 		}
 		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Clamp (pos.x, -27.0f, 47.0f);
+		pos.y = Mathf.Clamp (pos.y, -8.0f, 8.0f);
+		transform.position = pos;
 	}
 }
--- Prototype/Assets/Scripts/Tests/CameraMoveTest.cs
+++ Prototype/Assets/Scripts/Tests/CameraMoveTest.cs
@@ -15,12 +15,14 @@ public class CameraMoveTest : MonoBehaviour {
 		if(Input.GetMouseButtonDown (0)){
 			target = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zPos));
 			target.z = transform.position.z;
+
+			if(!Mathf.Approximately (gameObject.transform.position.magnitude, target.magnitude)){
+				Vector3 relativePos = target - transform.position ;
+				transform.rigidbody2D.AddForce(relativePos.normalized * speed);
+			}
 		}
 
-		if(!Mathf.Approximately (gameObject.transform.position.magnitude, target.magnitude)){
-			Vector3 relativePos = target - transform.position ;
-			transform.rigidbody2D.AddForce(relativePos.normalized * speed);
-		}
+
 
 		//Debug.Log (target);
 		Debug.Log (transform.position);
