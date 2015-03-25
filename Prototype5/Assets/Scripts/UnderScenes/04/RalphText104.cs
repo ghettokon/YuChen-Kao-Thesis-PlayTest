f92c437e18af9060385ffcb08b2cfcd68d648536
lindayckao <lindayckao@gmail.com>
2015-03-24 21:26:22 -0400

    polish 05/ 04

---
+++ Prototype5/Assets/Scripts/UnderScenes/04/RalphText104.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RalphText104 : MonoBehaviour {
+
+	string dialogue = ">Hey Zoe!";
+	
+	// Update is called once per frame
+	void Update () {
+		string textBuffer = dialogue;
+		if (dialogue == ">Hey Zoe!") {
+			textBuffer +="\nSo happy to see you!";
+			textBuffer +="\nAny plans for Christmas Eve?";
+		}
+		GetComponent<TextMesh>().text = textBuffer;
+	}
+}
