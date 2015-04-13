5c24436bd1a71e7f97ea071ae5ca093254a1ce9f
lindayckao <lindayckao@gmail.com>
2015-04-12 21:39:57 -0400

    revised Logo & main Cursor

--- Prototype5/Assets/Scripts/Camera/CameraMoving.cs
+++ Prototype5/Assets/Scripts/Camera/CameraMoving.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraMoving : MonoBehaviour {
 
-	public float Boundary = 50f;
+	public float Boundary = 70f;
 	public float speed = 5f;
 
 	public float theScreenWidth;
@@ -11,6 +11,11 @@ public class CameraMoving : MonoBehaviour {
 
 	private Vector3 currentLoc;
 	public float zPos = 6f;
+
+	public Texture2D cursorUpImage;
+	private bool showUpCursor = false;
+
+
 	
 	void Start () {
 		theScreenWidth = Screen.width;
@@ -18,29 +23,45 @@ public class CameraMoving : MonoBehaviour {
 	}
 
 	void Update () {
+
+
+
+
+		//right
 		if (Input.mousePosition.x > theScreenWidth - Boundary){
 			currentLoc = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zPos));
 			currentLoc.z = transform.position.z;
 			transform.position = Vector3.MoveTowards(transform.position, currentLoc, speed * Time.deltaTime);
 		}
-		
-		if (Input.mousePosition.x < 0 + Boundary){
+
+		//left
+		else if (Input.mousePosition.x < 0 + Boundary){
 			currentLoc = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zPos));
 			currentLoc.z = transform.position.z;
 			transform.position = Vector3.MoveTowards(transform.position, currentLoc, speed * Time.deltaTime);
 		}
-		
-		if (Input.mousePosition.y > theScreenHeight - Boundary){
+
+		//up
+		else if (Input.mousePosition.y > theScreenHeight - Boundary){
+			currentLoc = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zPos));
+			currentLoc.z = transform.position.z;
+			transform.position = Vector3.MoveTowards(transform.position, currentLoc, speed * Time.deltaTime);
+
+			//not working
+			showUpCursor = true;
+			if(showUpCursor) {
+				Cursor.SetCursor(this.cursorUpImage, Vector2.zero, CursorMode.Auto);
+			}
+		}
+
+		//down
+		else if (Input.mousePosition.y < 0 + Boundary){
 			currentLoc = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zPos));
 			currentLoc.z = transform.position.z;
 			transform.position = Vector3.MoveTowards(transform.position, currentLoc, speed * Time.deltaTime);
 		}
-		
-		if (Input.mousePosition.y < 0 + Boundary){
-			currentLoc = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zPos));
-			currentLoc.z = transform.position.z;
-			transform.position = Vector3.MoveTowards(transform.position, currentLoc, speed * Time.deltaTime);
-		}
+
+
 	}
 
 }
---
+++ Prototype5/Assets/Scripts/Camera/MainCursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCursor : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor(this.cursorTexture, Vector2.zero, CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Cursor.SetCursor(this.cursorTexture, Vector2.zero, CursorMode.Auto);
+	}
+}
